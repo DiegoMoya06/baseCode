568e04bcea0ccd8719df3051ecb3fc6717d0d8a0
DiegoMoya06 <diego0697ms@gmail.com>
2023-07-11 23:59:36 -0600

    Update BasecodeApplicationTests.java

--- src/test/java/com/dev/basecode/BasecodeApplicationTests.java
+++ src/test/java/com/dev/basecode/BasecodeApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BasecodeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	Tests will be added
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
